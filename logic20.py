b7bd4b5d2f376838fe2fb10421005ade5f51e901
unknown <princariens@gmail.com>
2022-03-17 21:15:12 +0500

    20 commit

--- logic20.py
+++ logic20.py
@@ -8,5 +8,31 @@ def main(n):
     Returns:
         bool: answer
     """
+    n1=n%10
+    n=n//10
 
-    return
+    n2=n%10
+    n=n//10
+
+    n3=n%10
+    n=n//10
+
+    n4=n%10
+    n=n//10
+
+    n5=n%10
+    n=n//10
+    one=0;zero=0
+    if n1==1:
+        one+=1
+    if n2==1:
+        one+=1
+    if n3==1:
+        one+=1
+    if n4==1:
+        one+=1
+    if n5==1:
+        one+=1
+    zero=5-one
+    return one>zero
+print(main(1100))
